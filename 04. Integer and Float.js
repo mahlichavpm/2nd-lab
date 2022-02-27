b6bb311cf5340d6d1299b4692841fe76733fe3c0
Viktor Yordanov <87126339+mahlichavpm@users.noreply.github.com>
2022-02-27 14:28:43 +0200

    problem 4 solved

--- 04. Integer and Float.js
+++ 04. Integer and Float.js
@@ -1,8 +1,4 @@
-function solve(num1) {
-  
-    let res = (num1 > 5) ? 'true' : 'false';
-    console.log(res);
-    
+function solve(num1,num2,num3) {
+    let sum = Number(num1+num2+num3);
+    console.log(sum + (sum % 1 === 0 ? ' - Integer' : ' - Float'));
 }
-
-solve('7')
