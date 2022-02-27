4e0df9cc6b6e75277107fdad291743967a59c30a
Viktor Yordanov <87126339+mahlichavpm@users.noreply.github.com>
2022-02-27 21:06:41 +0200

    update on solve no.9

--- 09. Special Numbers.js
+++ 09. Special Numbers.js
@@ -1,6 +1,28 @@
 function specialNumbers(num) {
 
+    function sumDigits(n){
+        let numArr = n.toString().split("");
+    
+        let sum = numArr.reduce(function(a, b){
+            return parseInt(a) + parseInt(b);
+        }, 0);
+        
+        return sum;
+    }
+
+    let flag = false;
+
     for(let i = 1; i <= num;i++) {
-        c
+        flag = false;
+       if (sumDigits(i) == 5 || sumDigits(i) == 7 || sumDigits(i) == 11){
+            flag = true;
+        }
+
+        if(flag) {
+            console.log(`${i} -> True`);
+        } else {
+            console.log(`${i} -> False`);
+        }
+        
     }
 }
